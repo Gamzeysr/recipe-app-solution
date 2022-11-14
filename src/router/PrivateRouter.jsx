38a4528f11743b9183ef0f302faca5298f396c86
Gamze <gamzeyasar35@icloud.com>
2022-11-14 12:51:17 +0300

    private

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -7,10 +7,12 @@ const PrivateRouter = () => {
   return (
     <div>
       {user ? <Outlet /> : <Navigate to="/login" />}
+      {/* 👆Burada diyorum ki kullanıcım varsa Outlete yönlendirecek eğer user yoksa geri login sauyfasına git login ol öğle gel diyorum .Ve geri yönlendirme yapıcagım için Navigate i kullanıyorum. Navigate to="/login" diyerek kullanıcıyı login sayfasına geri yönlendirmiş oldum. */}
     </div>
   )
 }
 
 export default PrivateRouter
 
-//!✨ Kullanıcının sayfaya giriş yapmadan  önce buraya yönlendridiğmiz kısım burası 
+//!✨ Kullanıcının sayfaya giriş yapmadan  önce buraya yönlendridiğmiz kısım burası
+//! 🎉🎀Kullanıcının izin verdiğimiz müddetce girecek oldugu kısım 
